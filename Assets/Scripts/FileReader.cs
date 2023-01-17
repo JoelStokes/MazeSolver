36e160466a5b3ce53ae760f1debc623461d3e827
Joel Stokes <joel.stokes.e@gmail.com>
2023-01-16 21:03:42 -0500

    Read maze from file & output GO grid

--- Assets/Scripts/FileReader.cs
+++ Assets/Scripts/FileReader.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 using System.IO;  
 
 public class FileReader : MonoBehaviour
 {
+    //UI Elements
     public TMP_InputField inputText;
+    public TextMeshProUGUI tipText;
+
     private string filePath;
     private string appPath;
     private bool parsing = false;
@@ -28,8 +32,8 @@ public class FileReader : MonoBehaviour
     public void CheckPath(){    //Make sure inputText is a valid filePath before parsing
         parsing = true;
 
-        if (inputText.text == null && inputText.text == ""){    
-            NoFileTyped();
+        if (inputText.text == null || inputText.text == ""){    
+            FileError("Please enter a file path");
         } else {
             if (inputText.text.Contains("/")){  //Check if they input whole file path or if it's just the text file inside the exe folder
                 filePath = inputText.text;
@@ -41,12 +45,19 @@ public class FileReader : MonoBehaviour
             if (!filePath.Contains(".txt")){
                 filePath += ".txt";
             }
-            
-            var sr = new StreamReader(filePath);
-            var fileContents = sr.ReadToEnd();
-            sr.Close();
-        
-            ParseFile(fileContents);
+
+            try //Check & read file at path
+            {
+                var sr = new StreamReader(filePath);    
+                var fileContents = sr.ReadToEnd();
+                sr.Close();
+                ParseFile(fileContents);
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex);
+                FileError(".txt file could not be found!");
+            }
         }
 
     }
@@ -62,9 +73,21 @@ public class FileReader : MonoBehaviour
         }
 
         gameManager.SetMaze(mazeGrid);
+
+        TurnOffFileUI();
     }
 
-    private void NoFileTyped(){ 
+    private void FileError(string errorMessage){     //A problem occured trying to read the file
+        tipText.text = errorMessage;
+        tipText.color = Color.red;
+        parsing = false;
+    }
 
+    private void TurnOffFileUI(){   //Once maze sent to Game Manager, hide File Input UI so maze drawing can start
+
+    }
+
+    public void ResetFileReader(){  //Called by GameManager when done with previous maze. Resets data & UI so next maze can be passed
+        parsing = false;
     }
 }
